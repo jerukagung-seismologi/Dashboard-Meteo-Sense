9b8292a7550efa2b33d6ba88a5790cb932f618b6
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-09-03 14:30:03 +0700

    fix: update text color in LoadingSpinner for improved readability

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => (
   <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
     <div className="w-12 h-12 border-8 border-emerald-500 border-t-transparent rounded-full animate-spin"></div>
-    <p className="ml-4 text-gray-700 text-2xl">Memuat Halaman...</p>
+    <p className="ml-4 text-gray-800 text-2xl">Memuat Halaman...</p>
   </div>
 );
 
