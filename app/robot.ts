bcfd4668c9d5cd92e23124891f15c07c907ee8e0
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-09-29 11:05:34 +0700

    refactor: update robots rules structure for clarity and consistency

--- app/robot.ts
+++ app/robot.ts
@@ -2,10 +2,16 @@ import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+    {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      }
+    ],
     sitemap: `https://meteo.jerukagunglabs.web.id/sitemap.xml`,
     host: `https://meteo.jerukagunglabs.web.id`,
   }
